f8bd7b8b29814de029b1582ab569c56cce20b682
aleksei-udalov <aleksei.udalov.01@gmail.com>
2017-09-08 17:28:38 +0400

    signing

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import binascii
+import requests
+import rlp
+import json
+from ethereum import transactions, utils
+
 from flask import Flask, request
 from flask_restful import Resource, Api
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -13,7 +19,20 @@ from models import Account
 
 class Signer(Resource):
     def post(self):
-        return {'status': 0}
+        req = request.get_json()
+        source = req['source']
+        dest = req.get('dest', '')
+        value = req.get('value', 0)
+        data = binascii.unhexlify(req.get('data', ''))
+        gasprice = 20 * 10 ** 9
+        startgas = 2 * 10 ** 6
+        account = db.session.query(Account).filter(Account.addr==source).limit(1).with_for_update().one()
+        priv = binascii.unhexlify(account.priv)
+        nonce = req['nonce']
+        tx = transactions.Transaction(nonce, gasprice, startgas, dest, value, data).sign(priv)
+        raw_tx = binascii.hexlify(rlp.encode(tx))
+#        r = requests.post('http://127.0.0.1:8545', json={'method': 'eth_sendRawTransaction', 'params': ['0x' + ra_tx.decode()], 'id': 1, 'jsonrpc': '2.0'}) 
+        return {'result': raw_tx.decode()}
 
 
 class KeyManager(Resource):
